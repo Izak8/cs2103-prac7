1ba8dd1770364edd0b7207a91171fd42b0489fdf
Izak8 <izak.baldacchino@outlook.com>
2023-05-15 12:20:36 +0930

    Add implementation for PrefixMatcher

--- PrefixMatcher.cpp
+++ PrefixMatcher.cpp
@@ -1,1 +1,28 @@
 #include "PrefixMatcher.h"
+
+int PrefixMatcher::selectRouter(std::string networkAddress)
+{
+	// Get words with a matching prefix.
+	std::vector<std::string> list = addressList.getWords(networkAddress);
+
+	// find the pair with the first address to be matched
+	auto i = routerMap.find(list.at(0));
+
+	// if the key doesn't exist return -1
+	if(i == routerMap.end())
+	{
+		return -1;
+	}
+
+	// otherwise return the router number with the corresponding address
+	return i->second;
+}
+
+void PrefixMatcher::insert(std::string address, int routerNumber)
+{
+	// insert the key-value pair (address, routerNumber) into the router-address map
+	routerMap.insert({address, routerNumber});
+
+	// insert the address into the addressList tree
+	addressList.insert(address);
+}
--- PrefixMatcher.h
+++ PrefixMatcher.h
@@ -1,4 +1,18 @@
 #ifndef PREFIX_MATCHER_H
 #define PREFIX_MATCHER_H
 
+#include <map>
+#include <string>
+#include "Trie.h"
+
+class PrefixMatcher
+{
+
+	std::map<std::string, int> routerMap;
+	Trie addressList;
+
+ public:
+	int selectRouter(std::string networkAddress);
+	void insert(std::string address, int routerNumber);
+};
 #endif
