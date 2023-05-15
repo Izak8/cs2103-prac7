8c324a177550277c09af36728d89132dc2ffc263
Izak8 <izak.baldacchino@outlook.com>
2023-05-15 10:56:32 +0930

    Add implementation for Trie::getWords

--- Trie.cpp
+++ Trie.cpp
@@ -102,13 +102,63 @@ Trie::Node* Trie::remove(Node* node, std::string const& key, size_t depth)
 	return node;
 }
 
-void Trie::printTerminal(std::string const& key)
+Trie::Node* Trie::traverse(std::string const& key)
 {
+	// Starting from root node
 	Node* node = root;
 
+	// Traverse to the node at the end of key
 	for(auto i = key.begin(); i != key.end(); ++i)
 	{
-		std::cout << node->isTerminal;
+		// For each character
+		// if node does not exist return null
+		if(node->child[*i] == nullptr)
+		{
+			return nullptr;
+		}
+
+		// otherwise traverse to next
 		node = node->child[*i];
 	}
+
+	// once traversal complete return node pointer
+	return node;
+}
+
+std::vector<std::string> Trie::getWords(std::string const& key)
+{
+	// traverse to the node at key
+	Node* node = traverse(key);
+ 	std::vector<std::string> list = {};
+
+	// base cases
+	// if the given key is a word
+	if(search(key) == true)
+	{
+		// push the key to the vector
+		list.push_back(key);
+
+		// if there are no more children
+		if(node->child.empty())
+		{
+			// return whole list
+			return list;
+		}
+	}
+
+	// recurisve call
+	// for each child of the current node
+	for(auto i : node->child)
+	{
+		char c = i.first;
+
+		// recursively getWords of the key + child character
+		std::vector<std::string> words = getWords(key+c);
+
+		// insert new words into the main vector
+		list.insert(list.end(),words.begin(), words.end());
+	}
+
+	// return the whole list
+	return list;
 }
